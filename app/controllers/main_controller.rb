18d2ac89b67ec24707d5060d6ae5df5b8c67de52
Hitoshi Saiwaki <hitoshi.ee@gmail.com>
2015-05-22 18:00:52 +0900

    fix root url

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,5 @@
 class MainController < ApplicationController
   def index
-    @client = Octokit::Client.new(access_token: Rails.application.secrets.github_access_token)
-    p @cilent.rate_limit
-    raise
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   resources :courses, only: [:show, :new, :create]
-  root to: "main#index"
+  root to: "courses#new"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
